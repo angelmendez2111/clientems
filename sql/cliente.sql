6c2b24e1f44dfc6b53e91d27094d8a44b1a737fc
angelmendez2111 <angelpieromendezquinones@gmail.com>
2025-02-22 00:57:32 -0500

    Primer commit api rest cliente

---
+++ sql/cliente.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cliente (
+    client_id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    direccion VARCHAR(255),
+    edad INT
+);
+`default`cliente
